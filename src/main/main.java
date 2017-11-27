0bbb705281a1f92f77a094fab788681045edf277
poonnanun <poonnanun1@gmail.com>
2017-11-27 21:37:28 +0700

    finish but still got somebug

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,14 @@
 package main;
-
+/**
+ * This programe is four in line game with gui.
+ * In Main menu there is Start,How to play and exit.
+ * This is my first project with gui so this is kind of hard to read.
+ * @author Ou's PC
+ *
+ */
 public class main {
-
+	public static void main(String[] args){
+		Start.mainMenu();
+	}
 }
+
